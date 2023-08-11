b7d4c901478a0e2776a550e182d46d638edf5eba
Zainulabdeen-101 <bbadshah691@gmail.com>
2023-08-11 15:58:25 +0500

    calculator project completed

--- app.js
+++ app.js
@@ -1,2 +1,54 @@
-console.log('this is me the first console');
-export {};
+import inquirer from "inquirer";
+//npm i --save-dev @types/inquirer use this command for inquirer types
+// using Asynchronous function because inquirer.prompt function return a Promise
+async function Calculate() {
+    // Used Object destructuring
+    const { operator, num1, num2 } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "operator",
+            choices: ["+", "-", "/", "*"],
+            message: "Choose one Operator",
+        },
+        {
+            type: "number",
+            name: "num1",
+            message: "Enter first number",
+        },
+        {
+            type: "number",
+            name: "num2",
+            message: "Enter Second number",
+        },
+    ]);
+    //Using Switch case conditional check for known outputs
+    switch (operator) {
+        case "+":
+            console.log(`The result of ${num1} + ${num2} = ${num1 + num2}`);
+            break;
+        case "-":
+            console.log(`The result of ${num1} - ${num2} = ${num1 - num2}`);
+            break;
+        case "/":
+            console.log(`The result of ${num1} / ${num2} = ${num1 / num2}`);
+            break;
+        case "-":
+            console.log(`The result of ${num1} / ${num2} = ${num1 * num2}`);
+            break;
+    }
+}
+// Calling the Calculate function
+async function repeat() {
+    do {
+        await Calculate();
+        var { repeat } = await inquirer.prompt([
+            {
+                type: 'confirm',
+                name: 'repeat',
+                message: 'Do you want to calculate again?'
+            }
+        ]);
+    } while (repeat === "Y" || repeat === "Yes");
+}
+;
+repeat();
--- app.ts
+++ app.ts
@@ -1,1 +1,65 @@
-console.log('this is me the first console')
+import inquirer from "inquirer";
+//npm i --save-dev @types/inquirer use this command for inquirer types
+
+//  using interface for creating the structure for the inquirer object and type checking
+interface Calculator {
+  operator: string;
+  num1: number;
+  num2: number;
+}
+
+// using Asynchronous function because inquirer.prompt function return a Promise
+async function Calculate() {
+  // Used Object destructuring
+  const { operator, num1, num2 }: Calculator = await inquirer.prompt([
+    {
+      type: "list",
+      name: "operator",
+      choices: ["+", "-", "/", "*"],
+      message: "Choose one Operator",
+    },
+    {
+      type: "number",
+      name: "num1",
+      message: "Enter first number",
+    },
+    {
+      type: "number",
+      name: "num2",
+      message: "Enter Second number",
+    },
+  ]);
+
+  //Using Switch case conditional check for known outputs
+  switch (operator) {
+    case "+":
+      console.log(`The result of ${num1} + ${num2} = ${num1 + num2}`);
+      break;
+    case "-":
+      console.log(`The result of ${num1} - ${num2} = ${num1 - num2}`);
+      break;
+    case "/":
+      console.log(`The result of ${num1} / ${num2} = ${num1 / num2}`);
+      break;
+    case "-":
+      console.log(`The result of ${num1} / ${num2} = ${num1 * num2}`);
+      break;
+  }
+}
+
+// Calling the Calculate function
+async function repeat() {
+  do {
+    await Calculate();
+    var { repeat } = await inquirer.prompt([
+      {
+        type: "confirm",
+        name: "repeat",
+        message: "Do you want to calculate again?",
+      },
+    ]);
+  } while (repeat === "Y" || repeat === "Yes");
+}
+
+// Calling the repeat  function
+repeat();
